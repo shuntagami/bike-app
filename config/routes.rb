36e20dfdeb19740b08275bc61eaf2df0c6f806d7
shuntagami <shuntagami23@gmail.com>
2020-10-01 21:42:43 +0900

    テーブル名変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "bikes#index"
+  root to: "posts#index"
 end
---
+++ db/migrate/20201001123417_change_bikes_to_posts.rb
@@ -0,0 +1,5 @@
+class ChangeBikesToPosts < ActiveRecord::Migration[6.0]
+  def change
+    rename_table :bikes, :posts
+  end
+end
