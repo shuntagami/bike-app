9f84114280cf5022b88709969e46a858130d3fbe
shuntagami <shuntagami23@gmail.com>
2020-10-02 21:38:59 +0900

    投稿の保存に成功

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,6 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:name,:description, :cc_id, :maker_id, :type_id).merge(user_id: current_user.id)
+    params.require(:post).permit(:image, :name, :description, :cc_id, :maker_id, :type_id).merge(user_id: current_user.id)
   end
 end
