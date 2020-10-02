1fc5d0a9da62ed9d6e26a47b9b16b493f1c5125e
shuntagami <shuntagami23@gmail.com>
2020-10-02 20:35:16 +0900

    newアクション、createアクション定義

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,12 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :cc
   belongs_to_active_hash :maker
   belongs_to_active_hash :type
+  
   has_one_attached :image
 
   #空の投稿を保存できないようにする
