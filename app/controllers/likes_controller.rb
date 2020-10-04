aad13a66c859adc5d8eb35313543c3545de51326
shuntagami <shuntagami23@gmail.com>
2020-10-04 20:13:38 +0900

    create, destroyアクション定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,21 @@
 class LikesController < ApplicationController
+  before_action :find_post
+
+  def create
+    @proposal = Proposal.find(params[:proposal_id])
+    @like = Like.new(user_id: current_user.id, proposal_id: params[:proposal_id])
+    @like.save
+  end
+
+  def destroy
+    @proposal = Proposal.find(params[:proposal_id])
+    @like = Like.find_by(user_id: current_user.id, proposal_id: params[:proposal_id])
+    @like.destroy
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
 end
