0e480359157ee232d4c6370bf2f1a4c887e900d8
shuntagami <shuntagami23@gmail.com>
2020-10-04 10:10:57 +0900

    show, createアクションの定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+  def create
+    comment = current_user.comments.build(comment_params)
+    if comment.save
+      redirect_to comment.post
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment, :post_id, :user_id)
+  end
 end
