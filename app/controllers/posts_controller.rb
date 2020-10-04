9490ec2204b17c76d505edad2d9f70cdb347aa61
shuntagami <shuntagami23@gmail.com>
2020-10-04 15:56:27 +0900

    comment一覧表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @comment = Comment.new
-    # @comments = @post.comments.includes(:user)
+    @comments = @post.comments.includes(:user)
   end
 
   private
