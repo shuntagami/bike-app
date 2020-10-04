436571ae990d530bb96ed1acfb00a822e1780f36
shuntagami <shuntagami23@gmail.com>
2020-10-04 12:00:54 +0900

    コメントの保存に成功

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,16 @@
 class CommentsController < ApplicationController
   def create
-    comment = current_user.comments.build(comment_params)
-    if comment.save
-      redirect_to comment.post
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@comment.post_id)
     end
+    # comment = Comment.create(comment_params)
+    # redirect_to post_path(comment.post_id) 
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:comment, :post_id, :user_id)
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
   end
 end
--- db/migrate/20201004001924_create_comments.rb
+++ db/migrate/20201004001924_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.text :comment, null: false
+      t.text :text, null: false
       t.references :post, foreign_key: true
       t.references :user, foreign_key: true
       t.timestamps
