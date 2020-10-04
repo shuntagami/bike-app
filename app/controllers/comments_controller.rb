1114848c1af87a20491bb300cc6af9eef384fdfb
shuntagami <shuntagami23@gmail.com>
2020-10-04 15:02:53 +0900

    Revert "エラーメッセージの表示"
    This reverts commit 1dd9a0494f7f12690b7a5db9584d5dcfc3ffbe5f.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,16 @@
 class CommentsController < ApplicationController
   def create
-    @post = Post.find(params[:post_id])
-    comment = Comment.new(comment_params)
-    if comment.save
-      redirect_to comment.post
-    else
-      redirect_to comment.post, flash: {
-        notice: comment,
-        error_messages: comment.errors.full_messages
-      }
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@comment.post_id)
     end
+    # comment = Comment.create(comment_params)
+    # redirect_to post_path(comment.post_id) 
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:comment).merge(user_id: current_user.id, post_id: params[:post_id])
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
   end
 end
