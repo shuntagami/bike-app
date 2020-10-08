90e37ee6dacd694b00b2730b36379e0b44089745
shuntagami <shuntagami23@gmail.com>
2020-10-08 19:24:12 +0900

    プロフィール画像を非同期通信で表示

--- app/javascript/channels/post_channel.js
+++ app/javascript/channels/post_channel.js
@@ -15,9 +15,10 @@ consumer.subscriptions.create("PostChannel", {
     `<div class="p-comment__item">
       ${data.comment.text}
       <div class="p-comment__bottomLine">
+        <a href="#"><img src="/assets/no-avatar.png" alt="avatar image" class="user_avatar"></a>
         <span> <a href = "/users/${data.comment.user_id}">${data.user.name}</a> </span>
         <span>${data.time}</span>
-        <span> <a data-confirm="コメントを削除してもよろしいですか？" rel="nofollow" data-method="delete" href="/tweets/${data.comment.post_id}/comments/${data.comment.id}">削除</a> </span>
+        <span> <a data-confirm="コメントを削除してもよろしいですか？" rel="nofollow" data-method="delete" href="/posts/${data.comment.post_id}/comments/${data.comment.id}">削除</a> </span>
       </div>
      </div>`;
     const comments = document.getElementById('comments');
