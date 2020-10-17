8eb8e72993470aa9397061893fe1d0acccc23062
shuntagami <shuntagami23@gmail.com>
2020-10-18 08:39:44 +0900

    action-cableの設定
    すべての送信元からのリクエストを許可

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,10 +39,7 @@ Rails.application.configure do
   config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://the-motorcycle.herokuapp.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'https://the-motorcycle.herokuapp.com/' ]
-  config.action_cable.allowed_request_origins = [ /http:\/\/.*/ ]
+  config.action_cable.disable_request_forgery_protection = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
