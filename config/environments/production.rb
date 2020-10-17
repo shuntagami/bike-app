eb5f588096fd2ccc3d5728e0f64d44dc8ecad5d7
shuntagami <shuntagami23@gmail.com>
2020-10-17 19:09:08 +0900

    不要な記述のコメントアウト

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
 
-  Mount Action Cable outside main process or domain.
+  # Mount Action Cable outside main process or domain.
   config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://the-motorcycle.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ 'https://the-motorcycle.herokuapp.com/' ]
