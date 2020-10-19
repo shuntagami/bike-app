964abacaade529990b5939aeed9a46ff17d379e4
shuntagami <shuntagami23@gmail.com>
2020-10-19 18:25:02 +0900

    編集機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
     resources :likes, only: [:create, :destroy]
   end
-  resources :users, only: [:show, :edit, :destroy]
+  resources :users, only: [:show, :edit, :update, :destroy]
 end
