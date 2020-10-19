b388462f5f4b8cf170db5bd5661084ff966d1801
shuntagami <shuntagami23@gmail.com>
2020-10-19 20:13:42 +0900

    avaterの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,10 +22,10 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :avatar)
   end
 
-   def find_user
+  def find_user
     @user = User.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :posts,    dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_one_attached :avatar
   # いいね機能用中間テーブル
   has_many :likes, dependent: :destroy
   has_many :like_posts, through: :likes, source: :post
