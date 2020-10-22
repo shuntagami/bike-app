aa8433a0575761895eee419738701d83b650ee53
shuntagami <shuntagami23@gmail.com>
2020-10-22 19:29:37 +0900

    jqueryをjavascriptに書き換え

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,21 +1,23 @@
-document.addEventListener('DOMContentLoaded', function(){
-  const fileField = document.getElementById("post_image");
-  fileField.addEventListener('change', function(e) {
-    const file = e.target.files[0];
-    const reader = new FileReader();
-    $preview = $("#img_field")
-    // const preview = document.getElementById('img_field');  
-    // var nest   = document.querySelector('image_tag');
+document.addEventListener('DOMContentLoaded', () => {
+  var fileField = document.getElementById("post_image");
+  fileField.addEventListener('change', (e) => {
+    var preview = document.getElementById('img_field');
+    var file = e.target.files[0];
+    var reader = new FileReader();  
     reader.readAsDataURL(file)
-    reader.onload = (function(file) {
-      return function(e) {
-        $preview.empty();
-        // preview.removeChild(nest);
-        $preview.append($('<img>').attr({
-          src: e.target.result,
-          class: "preview",
-          title: file.name
-        }));
+    reader.onload = ((file) => {
+      return (e) => {
+        // すでに存在している画像を消去
+        preview.innerHTML = null;
+
+        // imgタグを作成し、属性を追加
+        var img = document.createElement('img');
+        img.className = 'preview';
+        img.src = e.target.result;
+        console.log(e.target.result);
+
+        // 新しいimg要素を追加
+        preview.appendChild(img);
       };
     })(file);
     reader.readAsDataURL(file);
