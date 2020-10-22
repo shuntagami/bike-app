293096f2ec16ff34989ed674219df7f4ba05f8de
shuntagami <shuntagami23@gmail.com>
2020-10-22 16:38:54 +0900

    Update preview.js

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,22 +1,23 @@
-window.addEventListener('DOMContentLoaded', function(){
-  $(function(){
-    $fileField = $('#post_image')
-    $($fileField).on('change', $fileField, function(e) {
-      file = e.target.files[0]
-      reader = new FileReader(),
-      $preview = $("#img_field");
-
-      reader.onload = (function(file) {
-        return function(e) {
-          $preview.empty();
-          $preview.append($('<img>').attr({
-            src: e.target.result,
-            class: "preview",
-            title: file.name
-          }));
-        };
-      })(file);
-      reader.readAsDataURL(file);
-    });
+document.addEventListener('DOMContentLoaded', function(){
+  const fileField = document.getElementById("post_image");
+  fileField.addEventListener('change', function(e) {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    $preview = $("#img_field")
+    // const preview = document.getElementById('img_field');  
+    // var nest   = document.querySelector('image_tag');
+    reader.readAsDataURL(file)
+    reader.onload = (function(file) {
+      return function(e) {
+        $preview.empty();
+        // preview.removeChild(nest);
+        $preview.append($('<img>').attr({
+          src: e.target.result,
+          class: "preview",
+          title: file.name
+        }));
+      };
+    })(file);
+    reader.readAsDataURL(file);
   });
 });
