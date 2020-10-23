bbb5c18859d20b02c0006c73919e22737b4754d7
shuntagami <shuntagami23@gmail.com>
2020-10-23 21:31:35 +0900

    テストユーザーのavatarをseedファイルに作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 Faker::Config.locale = :ja
 
 # ユーザー作成
-99.times do |n|
+10.times do |n|
   name  = "テスト太郎#{n + 1}"
   email = "sample-#{n + 1}@example.com"
   password = "password"
+
   User.create!(name:  name,
                email: email,
                password:              password,
@@ -13,10 +14,17 @@ Faker::Config.locale = :ja
                updated_at: Time.zone.now)
 end
 
+# ユーザーAvatar生成
+users = User.order(:id).take(10)
+users.each_with_index do |user, i|
+user.avatar.attach(io: File.open("./db/fixtures/avatar/avatar-#{i + 1}.jpg"), filename: "avatar-#{i + 1}.jpg")
+user.save
+end
+
 # リレーションシップ
 users = User.all
 user  = users.first
-following = users[2..50]
-followers = users[3..40]
+following = users[2..10]
+followers = users[6..10]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
