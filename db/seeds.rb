c2fbc25e86d5edcc2211c06c67772a6e3d34b1c1
shuntagami <shuntagami23@gmail.com>
2020-10-23 17:02:25 +0900

    99人のテストユーザーを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Faker::Config.locale = :ja
+
+# ユーザー作成
+99.times do |n|
+  name  = "テスト太郎#{n + 1}"
+  email = "sample-#{n + 1}@example.com"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password,
+               created_at: Time.zone.now,
+               updated_at: Time.zone.now)
+end
+
+# リレーションシップ
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
