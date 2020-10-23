e236a0cd661189c5077d33e2a0a2dee1793e09c9
shuntagami <shuntagami23@gmail.com>
2020-10-23 16:01:09 +0900

    アソシエーション定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ApplicationRecord
   has_many :posts,    dependent: :destroy
+  # 自分がフォローしているユーザーとの関連
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  # 自分がフォローされるユーザーとの関連
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   has_many :comments, dependent: :destroy
   has_one_attached :avatar
   # いいね機能用中間テーブル
