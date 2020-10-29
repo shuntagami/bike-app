3d722497a3603597bb370dfbd0a8f75ebd606787
shuntagami <shuntagami23@gmail.com>
2020-10-29 11:21:18 +0900

    ajax通信（not json）

--- app/javascript/relationship.js
+++ app/javascript/relationship.js
@@ -1,22 +1,3 @@
-function relationship() {
-  const buttons = document.querySelectorAll(".follow_btn");
-  buttons.forEach(function (button) {
-    button.addEventListener("click", () => {
-      const userId = button.getAttribute("data-id");
-      const XHR = new XMLHttpRequest();
-      XHR.open("POST", `/users/${userId}/relationships`, true);
-      XHR.responseType = "json";
-      XHR.send();
-      XHR.onload = () => {
-       if (XHR.status != 200) {
-         alert(`Error ${XHR.status}: ${XHR.statusText}`);
-         return null;
-       }
-      };
-    });
-  });
-}
-document.addEventListener("DOMContentLoaded", relationship);
 
 
 
