9190e85a5efe0cae6c8bed8c899be081df3a310b
shuntagami <shuntagami23@gmail.com>
2020-11-01 17:28:04 +0900

    ログインに失敗した際に前のページに戻る

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,12 +1,27 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  def new_guest
-    user = User.guest
-    sign_in user
-    redirect_to root_path
-    flash[:success] = 'ゲストユーザーとしてログインしました。'
+  # def new_guest
+  #   user = User.guest
+  #   sign_in user
+  #   redirect_to root_path
+  #   flash[:success] = 'ゲストユーザーとしてログインしました。'
+  # end
+
+  def create
+    auth_options = { scope: resource_name, recall: "#{controller_path}#failed" }
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
   end
+
+  # ログイン失敗の時は直前のURLにリダイレクトする
+  def failed
+    redirect_to params[:user][:url]
+  end
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
