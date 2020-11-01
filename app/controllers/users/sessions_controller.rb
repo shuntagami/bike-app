350ed4d8cc6410308c1e1d7f34b6662425ec9290
shuntagami <shuntagami23@gmail.com>
2020-11-01 11:37:34 +0900

    ゲストログイン成功後のフラッシュメッセージ表示

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,7 +4,8 @@ class Users::SessionsController < Devise::SessionsController
   def new_guest
     user = User.guest
     sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+    redirect_to root_path
+    flash[:success] = 'ゲストユーザーとしてログインしました。'
   end
   # before_action :configure_sign_in_params, only: [:create]
 
