2c9c3e1552cc0d64c030ebb00dd03fc561fd06d4
shuntagami <shuntagami23@gmail.com>
2020-11-04 15:44:00 +0900

    新規登録失敗後にrootへ遷移

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,10 +4,26 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
   def create
-    super
-    @user = User.new
-    @user.build_bike
-    @user.save
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+        flash[:success] = "ようこそ！#{resource.name}さん"
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      redirect_to root_path, flash: {
+        user: resource,
+        error_messages: resource.errors.full_messages
+      }
+    end
   end
   # end
   # GET /resource/sign_up
@@ -45,6 +61,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
+ 
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, 
+  #     keys: [:name, bike_attributes: [:bike_name, :maker_id, :cc_id, :type_id]])
+  # end
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
   # end
