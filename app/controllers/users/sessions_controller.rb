79e57a332067b46399f1c2473b822f4c2eac8f75
shuntagami <shuntagami23@gmail.com>
2020-11-04 16:19:26 +0900

    不要な記述の削除

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
   # def new_guest
   #   user = User.guest
@@ -29,28 +27,4 @@ class Users::SessionsController < Devise::SessionsController
     respond_with resource, location: after_sign_out_path_for(resource)
     flash[:success] = "ログアウトしました"
   end
-
-  # before_action :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
