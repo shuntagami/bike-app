13814b78a28fefdd71fbcdfe203ff0cbaaa5a7ed
shuntagami <shuntagami23@gmail.com>
2020-11-05 17:02:40 +0900

    新規投稿画面をモーダルウィンドウ化

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,5 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy, :like]
-  def new
-    @post = Post.new
-  end
 
   def create
     @post = Post.new(post_params)
