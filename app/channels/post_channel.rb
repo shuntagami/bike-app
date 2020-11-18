4cb350f6a1f2f9e89e8b165e39f0c708af3cb33f
shuntagami <shuntagami23@gmail.com>
2020-11-18 22:22:14 +0900

    nginxリバースプロキシの設定

--- app/channels/post_channel.rb
+++ app/channels/post_channel.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-class PostChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from 'post_channel'
-  end
+# class PostChannel < ApplicationCable::Channel
+#   def subscribed
+#     stream_from 'post_channel'
+#   end
 
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-end
+#   def unsubscribed
+#     # Any cleanup needed when channel is unsubscribed
+#   end
+# end
