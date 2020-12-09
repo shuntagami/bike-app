29c9623eabeeb345dc0fb53e1f185b74300c50c9
shuntagami <shuntagami23@gmail.com>
2020-12-09 12:04:16 +0900

    投稿詳細画面の表示内容変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # PostのSelectBoxを定義
   WEATHERS = %w[快晴 晴れ 薄曇り 曇り 小雨 雨 豪雨 雷 みぞれ 雪 大雪 あられ ひょう 霧 霧雨 砂あらし].freeze
-  FEELINGS = %w[うだる暑さ 暑い 暖かい ちょうどいい 肌寒い 凍えるほど寒い あてはまらない].freeze
+  FEELINGS = %w[うだる暑さ 暑い 暖かい ちょうどいい 肌寒い 凍えるほど寒い ].freeze
   ROAD_CONDITION = %w[問題なし 一部凍っている ほとんど凍結している].freeze
 
   def resource_name
