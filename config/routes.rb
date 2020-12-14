92f76a5185667b7183736bc1d265bf6fca9f348c
shuntagami <shuntagami23@gmail.com>
2020-12-14 12:02:12 +0900

    deviseのパス名を変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,31 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
-  # devise_scope :user do
-  #   post 'users/guest_sign_in', to: 'users/sessions#new_guest'
-  # end
+
+  devise_for :users,
+              path: '',
+              path_names: {
+                sign_up: '',
+                sign_in: 'login',
+                sign_out: 'logout',
+                registration: 'signup'
+              },
+              controllers: {
+                registrations: 'users/registrations',
+                sessions: 'users/sessions'
+              }
+  devise_scope :user do
+    get 'signup', to: 'users/registrations#new'
+    get 'login', to: 'users/sessions#new'
+    get 'logout', to: 'users/sessions#destroy'
+  end
+
   resources :users do
     resource :relationships, only: %i[create destroy]
     member do
       get :following, :followers
     end
   end
+
   resources :posts do
     collection do
       get :cities_select
@@ -19,5 +35,6 @@ Rails.application.routes.draw do
     end
     resources :comments, only: %i[create destroy]
   end
+
   post 'posts/like/:id' => 'posts#like', as: 'like_posts'
 end
