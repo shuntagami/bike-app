68f00126b8de6612a46d5145b1043f932fabaca7
shuntagami <shuntagami23@gmail.com>
2020-12-15 16:50:28 +0900

    Create likes_spec.rb

---
+++ spec/system/likes_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+RSpec.describe 'Likes', type: :system do
+  let!(:user) do
+    create(:user,
+           :with_posts)
+  end
+  let!(:alice) do
+    create(:user,
+           name: 'Alice',
+           email: 'alice@example.com',
+           password: 'password_alice')
+  end
+
+  it '既存の投稿にいいね/いいね解除する', js: true do
+    visit root_path
+
+    # Aliceがログインする
+    click_link 'ログイン'
+    expect(current_path).to eq root_path
+    expect(page).to have_content '次回から自動的にログイン'
+
+    fill_in 'メールアドレス', with: 'alice@example.com'
+    fill_in 'パスワード', with: 'password_alice'
+    click_button 'ログイン'
+    expect(current_path).to eq root_path
+
+    # 記事詳細へ移動する
+    # click_link '新着投稿'
+    post = user.posts.first
+    expect(page).to have_link 'a', href: "/posts/#{post.id}"
+    find('.img').click
+    expect(current_path).to eq "/posts/#{post.id}"
+
+    # 投稿にいいね！する
+    expect(page).to have_selector '.fa-heart'
+    expect do
+      click_link '0'
+      expect(page).to have_selector '.fa-heart'
+      # expect(page).to_not have_selector '.fa-heart'
+    end.to change(post.likes, :count).by(1)
+
+    # マイページに移動する→記事詳細へ移動する
+    visit user_path(alice)
+    expect(current_path).to eq "/users/#{alice.id}"
+    expect(page).to have_content 'いいね 1件'
+    click_link 'いいね'
+    find('.img').click
+    expect(current_path).to eq "/posts/#{post.id}"
+
+    # いいね！解除する
+    expect(page).to have_selector '.fa-heart'
+    expect do
+      click_link '1'
+      expect(page).to have_selector '.fa-heart'
+      # expect(page).to_not have_selector '.fa-heart'
+    end.to change(post.likes, :count).by(-1)
+
+    # マイページに移動する
+    visit user_path(alice)
+    expect(page).to_not have_content 'いいね 1件'
+    expect(page).to have_content 'いいね 0件'
+  end
+end
